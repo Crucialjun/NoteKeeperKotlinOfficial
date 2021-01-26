2e77bbd70a263f31d9bfd911119494597f5c674b
Crucialjun <nich.otieno@gmail.com>
2021-01-26 06:17:46 +0300

    Initial commit

---
+++ app/src/main/java/com/example/notekeeperkotlinofficial/MainActivity.kt
@@ -0,0 +1,48 @@
+package com.example.notekeeperkotlinofficial
+
+import android.os.Bundle
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.TextView
+import kotlinx.android.synthetic.main.content_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        setSupportActionBar(findViewById(R.id.toolbar))
+
+        findViewById<FloatingActionButton>(R.id.fab).setOnClickListener { view ->
+            val originalValue = txt_displayed_value.text.toString().toFloat()
+
+            val newValue = originalValue * 2
+
+            txt_displayed_value.text = newValue.toString()
+
+
+            Snackbar.make(view,
+                "Value $originalValue change to $newValue",
+                Snackbar.LENGTH_LONG).show()
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        return when (item.itemId) {
+            R.id.action_settings -> true
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+}
