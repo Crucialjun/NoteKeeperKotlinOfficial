921702b3562f5a13158134413963ca4df7fe5364
Crucialjun <nich.otieno@gmail.com>
2021-01-27 23:34:31 +0300

    Pass Data

--- app/src/main/java/com/example/notekeeperkotlinofficial/NoteListActivity.kt
+++ app/src/main/java/com/example/notekeeperkotlinofficial/NoteListActivity.kt
@@ -2,10 +2,14 @@ package com.example.notekeeperkotlinofficial
 
 import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.material.floatingactionbutton.ExtendedFloatingActionButton
+import kotlinx.android.synthetic.main.content_note_list.*
 
 class NoteListActivity : AppCompatActivity() {
 
@@ -16,6 +20,19 @@ class NoteListActivity : AppCompatActivity() {
 
         findViewById<ExtendedFloatingActionButton>(R.id.fab).setOnClickListener { view ->
             val intent = Intent(this,MainActivity::class.java)
+            startActivity(intent)
+        }
+
+        listNotes.adapter =
+                ArrayAdapter(
+                        this,
+                        android.R.layout.simple_list_item_1,
+                        DataManager.notes
+                )
+
+        listNotes.setOnItemClickListener { parent, view, position, id ->
+            val intent = Intent(this,MainActivity::class.java)
+            intent.putExtra(EXTRA_NOTE_POSITION,position)
             startActivity(intent)
         }
     }
